72ce05b02f194791c44e8b88906d20bfad64ded7
Matheus Schroder Bereta <matheus_bereta@hotmail.com>
2024-07-04 12:11:13 -0300

    test: Adding AppiumActions unit tests

--- src/main/java/com/appium/arguments/BoundingArea.java
+++
@@ -1,3 +0,0 @@
-package com.appium.arguments;
-
-public record BoundingArea(int left, int top, int width, int height) { }
---
+++ src/test/java/com/appium/AppiumActionsTest.java
@@ -0,0 +1,82 @@
+package com.appium;
+
+import com.appium.actions.*;
+import io.appium.java_client.AppiumDriver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+class AppiumActionsTest {
+
+    @Mock
+    private AppiumDriver driver;
+
+    @Mock
+    private Capabilities capabilities;
+
+    private AppiumActions appiumActions;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        when(driver.getCapabilities()).thenReturn(capabilities);
+        when(driver.getCapabilities().getPlatformName()).thenReturn(Platform.ANDROID);
+
+        appiumActions = new AppiumActions(driver);
+    }
+
+    @DisplayName("Should throws a IllegalArgumentException when driver is null")
+    @Test
+    void testDriverNull() {
+        assertThrows(IllegalArgumentException.class, () -> new AppiumActions(null));
+    }
+
+    @DisplayName("Should return a instance of DeviceActions")
+    @Test
+    void testDeviceActions() {
+        DeviceActions deviceActions = appiumActions.deviceActions;
+
+        assertInstanceOf(DeviceActions.class, deviceActions);
+    }
+
+    @DisplayName("Should return a instance of SessionActions")
+    @Test
+    void testSessionActions() {
+        SessionActions sessionActions = appiumActions.sessionActions;
+
+        assertInstanceOf(SessionActions.class, sessionActions);
+    }
+
+    @DisplayName("Should return a instance of GestureActions")
+    @Test
+    void testGestureActions() {
+        GestureActions gestureActions = appiumActions.gestureActions;
+
+        assertInstanceOf(GestureActions.class, gestureActions);
+    }
+
+    @DisplayName("Should return a instance of TapActions")
+    @Test
+    void testTapActions() {
+        TapActions tapActions = appiumActions.tapActions;
+
+        assertInstanceOf(TapActions.class, tapActions);
+    }
+
+    @DisplayName("Should return a instance of KeyboardActions")
+    @Test
+    void testKeyboardActions() {
+        KeyboardActions keyboardActions = appiumActions.keyboardActions;
+
+        assertInstanceOf(KeyboardActions.class, keyboardActions);
+    }
+}
