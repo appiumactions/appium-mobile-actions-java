2725cc9c09ec35be87025143195d156cca2713c1
Matheus Schroder Bereta <matheus_bereta@sicredi.com.br>
2024-10-03 17:41:59 -0300

    docs: Adding Github Pages

--- .github/docs/docusaurus.config.ts
+++ .github/docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/appium-logo-horiz.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://appiumactions.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -17,8 +17,10 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'appiumactions', // Usually your GitHub org/user name.
   projectName: 'appium-mobile-actions-java', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
